5f3f1f77475801ce4ebf4473581894ff1ca14979
Dan <bmdmartinez@gmail.com>
2021-05-31 12:38:20 -0400

    Created/executed basic functions, interacted with player, stored data

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! Javascript is running!");
+ var playerName = window.prompt("What is your robot's name?");
+ console.log(playerName); //stores the data on programmer end, accessed via dev tools in browswer
+ console.log("This logs a string, good for leaving yourself a message");
+ console.log(10 + 10); //this will do math and log 20
+ console.log("Our robot's name is " + playerName);
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
