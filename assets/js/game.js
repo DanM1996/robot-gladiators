a04225a62de253d6c99a8620515dfed1e40e845e
Dan <bmdmartinez@gmail.com>
2021-06-05 14:43:45 -0400

    randomized who attacks first

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,17 +25,35 @@ var fightOrSkip = function() {
     }
 // fight function (now with parameter for enemy's name)  
 var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
     while (playerInfo.health > 0 && enemy.health > 0) { // so long as this is true, this runs
-      if (fightOrSkip()) {
-        // if true, break the while loop and return to the for loop
-        break;
-      }
-      // generate random damage value based on player's attack power
+      if (isPlayerTurn) {
+        // as player if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+          // if true, leave fight by breaking loop
+          break;
+        }
+
+      
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
       // remove enemy's health by subtracting the amount set in the attack variable
       enemy.health = Math.max(0, enemy.health - damage);
       console.log(
-        playerInfo.name + ' attacked ' + enemy.name + '. ' + enemy.name + ' now has ' + enemy.health + ' health remaining.'
+        playerInfo.name + 
+        ' attacked ' + 
+        enemy.name + 
+        '. ' + 
+        enemy.name + 
+        ' now has ' + 
+        enemy.health + 
+        ' health remaining.'
       );
   
       // check enemy's health
@@ -50,12 +68,20 @@ var fight = function(enemy) {
       } else {
         window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
       }
-  
+      // player gets attacked first
+    } else {
       var damage = randomNumber(enemy.attack - 3, enemy.attack);
       // remove players's health by subtracting the amount set in the enemy Attack variable
       playerInfo.health  = Math.max(0, playerInfo.health  - damage);
       console.log(
-        enemy.name + ' attacked ' + playerInfo.name + '. ' + playerInfo.name + ' now has ' + playerInfo.health  + ' health remaining.'
+        enemy.name + 
+        ' attacked ' + 
+        playerInfo.name + 
+        '. ' + 
+        playerInfo.name + 
+        ' now has ' + 
+        playerInfo.health  + 
+        ' health remaining.'
       );
   
       // check player's health
@@ -66,6 +92,8 @@ var fight = function(enemy) {
       } else {
         window.alert(playerInfo.name + ' still has ' + playerInfo.health  + ' health left.');
       }
+    }
+    isPlayerTurn = !isPlayerTurn;
     }
   };
   
