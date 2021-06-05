9de8b0d3fabfe440f11dc4f2c2db08fdec9e304e
Dan <bmdmartinez@gmail.com>
2021-06-05 14:45:11 -0400

    randomized enemy and player attack order

--- assets/js/game.js
+++ assets/js/game.js
@@ -108,7 +108,7 @@ var fight = function(enemy) {
       if (playerInfo.health  > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-        debugger;
+        
         // pick new enemy to fight based on the index of the enemy Names array
         var pickedEnemyObj = enemyInfo[i];
   
